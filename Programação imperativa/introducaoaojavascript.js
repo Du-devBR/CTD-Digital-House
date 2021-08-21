611c92fab45995e073760b4d8cef57c8b582b081
Du-devBR <89052479+Du-devBR@users.noreply.github.com>
2021-08-21 14:39:52 -0300

    Criando pasta para arquivos das aulas

---
+++ Programação imperativa/introducaoaojavascript.js
@@ -0,0 +1,56 @@
+// let cumprimento = 'bom dia';
+// // console.log(cumprimento);
+
+
+// cumprimento = 'tchau';
+// // console.log(cumprimento)
+
+// if(true){
+//     let cumprimento = 'Eduardo';
+//     console.log(cumprimento)
+// }
+
+//const carro = "civic";
+
+//arro = 'fox';
+//console.log(carro)
+
+// let primeiroNumero = 10;
+// let segundoNumero = 2;
+// console.log(soma = primeiroNumerosegundoNumero)
+
+// *******TIPOS DE DADOS**********
+
+// ***string pode usar "" ou ''***
+// let nome = 'Eduardo'
+// console.log(nome)
+
+// ***numbers*** 
+// **Inteiros
+// let idade = 28;
+// console.log(idade)
+
+// **decimal usar . e não ,**
+// let altura = 1.78;
+// console.log(altura)
+
+// ***booleanos TRUE***
+// let diaEstaChuvoso = true;
+// console.log(diaEstaChuvoso)
+
+// ***booleanos False***
+// let diaEstaChuvoso = false;
+// console.log(diaEstaChuvoso)
+
+// ***Valor not a number ou NaN***
+// let naoNumerico = NaN;
+// console.log(naoNumerico)
+
+// ***Valor null***
+// let jogadores = null;
+// console.log(jogadores)
+
+// ***Valor undefined***
+// let espectadores = undefined;
+// espectadores = 10;
+// console.log(espectadores)
