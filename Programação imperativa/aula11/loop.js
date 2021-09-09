3287d1510eeeaa13873765b12d46c8fd1a5c7f74
Du-devBR <89052479+Du-devBR@users.noreply.github.com>
2021-09-08 21:47:57 -0300

    Trabalhando com WHILE

--- Programação imperativa/aula11/loop.js
+++ Programação imperativa/aula11/loop.js
@@ -14,33 +14,87 @@
 // 1. Repetir como um papagaio
 // Crie um loop utilizando for que se repita 5 vezes. Dentro de cada repetição se deve mostrar no console a mensagem “Olá mundo”.
 
-for(var i = 0; i <=4; i++){
-    console.log('Olá mundo')
-}
+// for(var i = 0; i <=4; i++){
+//     console.log('Olá mundo')
+// }
 
-// 2. Contando números ímpares
-// Crie um loop for de 1 a 10, em que exiba no console apenas o valor das repetições de números ímpares. Dica: A ideia é que nas repetições ímpares de 0 ao 10 (1,3,5,7,9) sejam mostrados os números das repetições no console.
+// // 2. Contando números ímpares
+// // Crie um loop for de 1 a 10, em que exiba no console apenas o valor das repetições de números ímpares. Dica: A ideia é que nas repetições ímpares de 0 ao 10 (1,3,5,7,9) sejam mostrados os números das repetições no console.
 
-var impar = [1, 3, 5, 7, 9 ]
-var par = [2, 4, 6, 8, 10]
+// var impar = [1, 3, 5, 7, 9 ]
+// var par = [2, 4, 6, 8, 10]
 
-for(var i = impar; i <= impar; i++){
-    console.log(impar)
-}
+// for(var i = impar; i <= impar; i++){
+//     console.log(impar)
+// }
 
-// 3. Criando a tabuada
+// // 3. Criando a tabuada
 
-// Crie uma tabuada utilizando o for. Dica: Para essa tarefa você irá precisar utilizar dois loops (for).
+// // Crie uma tabuada utilizando o for. Dica: Para essa tarefa você irá precisar utilizar dois loops (for).
 
 
-function tabuada(){
-    var valorTabuada = prompt('Qual o valor voce que mulplicar')
-    for(var i = 1; i <=10; i++){
-        console.log(valorTabuada + 'x' + i + '=' + (valorTabuada*i))
-    }
+// function tabuada(){
+//     var valorTabuada = prompt('Qual o valor voce que mulplicar')
+//     for(var i = 1; i <=10; i++){
+//         console.log(valorTabuada + 'x' + i + '=' + (valorTabuada*i))
+//     }
 
-}
-tabuada()
+// }
+// tabuada()
+
+
+
+
+// Exercicios While
+// 1. Programa em Javascript que mostra os números ímpares
+//Escreva um aplicativo em Javascript que mostre todos os números ímpares de 1 até 100.
+
+// function numeroImpares(){
+//     var impar = 1
+//     while(impar <=100){
+//         console.log(impar)
+//         impar+=2
+//     }
+// }
+// numeroImpares()
+
+// // 2. Programa em Javascript que mostra os números pares</li>
+// // Escreva um aplicativo em Javascript que mostre todos os números pares de 1 até 100.
+
+// function numerosPares(){
+//     var par = 2
+//     while(par <=100){
+//         console.log(par)
+//         par+=2
+//     }
+// }
+// numerosPares()
+
+// 3. Programa em Javascript que mostra os números pares e ímpares
+// Escreva um aplicativo em Javascript que recebe um número inteiro e mostra os números pares e ímpares (separados), de 1 até esse inteiro.
+
+
+// var pergunta = prompt('Digite um numero');
+// function numerosImpar(){
+//     var impar = 1;
+//     while(impar <= pergunta){
+//         console.log(impar)
+//         impar+=2
+//     }
+
+// }
+// function numerosPar(){
+//     var par = 2;
+//     while(par <= pergunta){
+//         console.log(par)
+//         par+=2
+//     }
+
+// }
+// numerosImpar()
+// numerosPar()
+
+
 
 
 
